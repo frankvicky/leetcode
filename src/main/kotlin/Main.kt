06889f90bfd6e7cc526cdb475d1df14b187ff55b
kit <tingiao.ki@ttpsc.com>
2022-11-04 00:22:04 +0800

    Add array and string explore card related solution

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,20 @@
+import ting.leetcode.arrayandstring.FindPivotIndex
+import ting.leetcode.arrayandstring.LargestNumberAtLeastTwiceOfOthers
 import ting.leetcode.arrays101.*
 import ting.leetcode.beginnerguide.FindAllNumbersDisappearedInAnArray
 import ting.leetcode.beginnerguide.RansomNote
 import ting.leetcode.beginnerguide.ThirdMaximumNumber
 
 fun main(args: Array<String>) {
-    val solution = FindAllNumbersDisappearedInAnArray()
+    val solution = PlusOne()
 //    val numbers = intArrayOf(1,1)
-    val numbers = intArrayOf(4,3,2,7,8,2,3,1)
+//    val numbers = intArrayOf(1,7,3,6,5,6)
+//    val numbers = intArrayOf(9,8,7,6,5,4,3,2,1,0)
+    val numbers = intArrayOf(0,1,2,9)
+//    println(numbers.joinToString(""))
+//    val numbers = intArrayOf(1, 2, 3)
 //    val numbers = listOf(17,18,5,4,6,1)
-    println(solution.findDisappearedNumbers(numbers))
+    println(solution.plusOneSimpleSolution(numbers).contentToString())
 //    println(solution.replaceElements(intArrayOf(17,18,5,4,6,1)).contentToString())
 
 }
---
+++ src/main/kotlin/ting/leetcode/arrayandstring/FindPivotIndex.kt
@@ -0,0 +1,28 @@
+package ting.leetcode.arrayandstring
+
+class FindPivotIndex {
+    fun pivotIndex(nums: IntArray): Int {
+        for (pivotIndex in nums.indices) {
+            var rightSum = 0
+            var leftSum = 0
+            for (leftIndex in 0 until pivotIndex) {
+                leftSum += nums[leftIndex]
+            }
+            for (rightIndex in (pivotIndex + 1) .. nums.lastIndex) {
+                rightSum += nums[rightIndex]
+            }
+            if (leftSum == rightSum) return pivotIndex
+        }
+        return -1
+    }
+
+    fun pivotIndexWithPivotValue(nums: IntArray): Int {
+        val sum = nums.sum()
+        var leftSum = 0
+        for ((pivotIndex, value) in nums.withIndex()) {
+            if (leftSum == sum - value - leftSum) return pivotIndex
+            leftSum += value
+        }
+        return -1
+    }
+}
