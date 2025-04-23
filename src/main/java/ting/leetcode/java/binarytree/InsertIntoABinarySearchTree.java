e2410996f0536bc9b971fb4a04a6526cdc13bfc2
frankvicky <kitingiao@gmail.com>
2025-04-23 17:55:36 +0800

    solve some questions

---
+++ src/main/java/ting/leetcode/java/binarytree/InsertIntoABinarySearchTree.java
@@ -0,0 +1,18 @@
+package ting.leetcode.java.binarytree;
+
+public class InsertIntoABinarySearchTree {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+
+        if (root.val > val) {
+            root.left = insertIntoBST(root.left, val);
+        }
+        if (root.val < val) {
+            root.right = insertIntoBST(root.right, val);
+        }
+
+        return root;
+    }
+}
